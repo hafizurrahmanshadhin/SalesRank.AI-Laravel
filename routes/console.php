f836d7afe070015f3871c707e8a7e060254ff0a2
hafizurrahmanshadhin <hr.shadhin36@gmail.com>
2025-07-01 15:33:39 +0600

    ...

--- routes/console.php
+++ routes/console.php
@@ -4,12 +4,12 @@ use App\Console\Commands\MakeInterface;
 use App\Console\Commands\MakeService;
 use Illuminate\Support\Facades\Artisan;
 
-//# registering make service class command
+// registering make service class command
 Artisan::command('make:service {name}', function ($name) {
     $this->call(MakeService::class, ['name' => $name]);
 });
 
-//~ registering make interface class command
+// registering make interface class command
 Artisan::command('make:interface {name}', function ($name) {
     $this->call(MakeInterface::class, ['name' => $name]);
 });
